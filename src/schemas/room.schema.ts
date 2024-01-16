80b9718d8d614f777f9d67561bfd3665a30a1f56
Aniruddh Dev Upadhyay <aniruddh622003@gmail.com>
2024-01-17 02:31:11 +0530

    Updated Room Schema for Seating Plan

--- src/schemas/room.schema.ts
+++ src/schemas/room.schema.ts
@@ -18,6 +18,26 @@ export class Room {
     default: null,
   })
   room_invigilator_id: string | null;
+
+  // TODO: Add UFM Details
+  @Prop({
+    default: [],
+  })
+  student: [
+    {
+      sap_id: number;
+      roll_no: string;
+      student_name: string;
+      course: string;
+      subject: string;
+      subject_code: string;
+      eligible: {
+        type: string;
+        enum: ['YES', 'F_HOLD', 'DEBARRED'];
+        default: 'YES';
+      };
+    },
+  ];
 }
 
 export const RoomSchema = SchemaFactory.createForClass(Room);
